90edbaab48f88ee5f1b1cc5d633ad4d802a9da3a
Chris Phillips <chris.is.phillips@gmail.com>
2015-11-27 09:49:16 +0700

    File Name Changes

---
+++ BeginnerBonfires/08 Confirm the Ending.js
@@ -0,0 +1,11 @@
+function end(str, target) {
+
+  if (str.substr(str.length - target.length, target.length) === target) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+end("Bastian", "n");
